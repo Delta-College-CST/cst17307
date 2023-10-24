be7673e0a5e66061012daccf33e48fcfe3e5710e
teklingl <teklingl@gmail.com>
2023-10-24 11:50:25 -0400

    Study/Practice

--- salutation.py
+++ salutation.py
@@ -1,18 +1,6 @@
-# This program generates a random salutation.
+def sayMessage(salutation):
+  print(salutation)
 
-def sayMessage():
-    import random
-    selection = random.randrange(1, 4)
-    if selection == 0:
-        returnMsg = "Hey"
-    elif selection == 1:
-        returnMsg = "Yo"    
-    elif selection == 2:
-        returnMsg = "Wazzup"    
-    elif selection == 3:
-        returnMsg = "How goes it?"
-    return returnMsg
-
-print(sayMessage())
-
-    
+sayMessage("Hey")
+sayMessage("Yo")
+sayMessage("Wazzup")
